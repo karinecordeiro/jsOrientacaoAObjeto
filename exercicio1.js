92b8089ed5ea24968a5c9b54a663e86f42d3bde3
Karine C <karine.cordeiro1998@hotmail.com>
2023-02-27 16:25:25 -0300

    primeiro exercicio de orientação a objeto em JS

---
+++ exercicio1.js
@@ -0,0 +1,15 @@
+class cliente{
+    nome;
+    cpf;
+    agencia;
+    saldo;
+}
+
+const cliente1 = new cliente();
+
+cliente1.nome = "Alice";
+cliente1.cpf = 11122233300;
+cliente1.agencia = 1001;
+cliente1.saldo = 20;
+
+console.log(cliente1);
